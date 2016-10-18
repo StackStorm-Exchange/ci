5075607b8dade771d70997a87132110f4ce6a5c7
Edward Medvedev <edward.medvedev@gmail.com>
2016-10-18 14:06:44 +0700

    Make curl quit on failures, fix API requests, change git init order

--- utils/exchange-bootstrap.sh
+++ utils/exchange-bootstrap.sh
@@ -25,60 +25,71 @@ then
 	exit 1;
 fi
 
-PACK=$1
-EXCHANGE_ORG=${EXCHANGE_ORG:-StackStorm-Exchange}
-EXCHANGE_PREFIX=${EXCHANGE_PREFIX:-stackstorm}
-REPO_NAME=${EXCHANGE_PREFIX}-${PACK}
-REPO_DIR=/tmp/${REPO_NAME}
-REPO_URL=https://${USERNAME}:${PASSWORD}@github.com/${EXCHANGE_ORG}/${REPO_NAME}
+PACK="$1"
+EXCHANGE_ORG="${EXCHANGE_ORG:-StackStorm-Exchange}"
+EXCHANGE_PREFIX="${EXCHANGE_PREFIX:-stackstorm}"
+REPO_NAME="${EXCHANGE_PREFIX}-${PACK}"
+REPO_DIR="/tmp/${REPO_NAME}"
+REPO_URL="https://${USERNAME}:${PASSWORD}@github.com/${EXCHANGE_ORG}/${REPO_NAME}"
 
 # Check if the repo exists
-git ls-remote ${REPO_URL} > /dev/null 2>&1
-if [ "$?" == 0 ]; then
+
+if git ls-remote ${REPO_URL} > /dev/null 2>&1;
+then
 	echo "The repository already exists, cannot bootstrap."
 	exit 1
 fi
 
-# Git: init an empty repo and set the remote
-rm -rf ${REPO_DIR}
+# Git: create an empty repo and set the remote
+rm -rf ${REPO_DIR} /tmp/${PACK}_rsa* /tmp/${PACK}_user_token
 mkdir ${REPO_DIR} && cd ${REPO_DIR}
-git init && git remote set-url origin ${REPO_URL}
-
-# Git: push circle.yml
-wget https://raw.githubusercontent.com/StackStorm-Exchange/ci/master/.circle/circle.yml.sample -O circle.yml
-git add circle.yml
-git commit -m 'Bootstrapping a StackStorm Exchange pack repository.'
-git push origin master
+git init && git remote add origin ${REPO_URL}
 
 # Generate a keypair
 ssh-keygen -b 2048 -t rsa -f /tmp/${PACK}_rsa -q -N ""
 
+# GitHub: create a repo
+curl -sS --fail -u "${USERNAME}:${PASSWORD}" -X POST --header "Content-Type: application/json" \
+	-d '{"name": "'"${REPO_NAME}"'"}' \
+	"https://api.github.com/orgs/${EXCHANGE_ORG}/repos"
+
 # GitHub: create a read-write key for the repo
-curl -X POST --header "Content-Type: application/json" \
-	-d '{"title": "CircleCI read-write key", "key": "$(cat /tmp/${PACK}_rsa.pub)", "read_only": false}' \
-	POST https://api.github.com/repos/${EXCHANGE_ORG}/${REPO_NAME}/keys
+curl -sS --fail -u "${USERNAME}:${PASSWORD}" -X POST --header "Content-Type: application/json" \
+	-d '{"title": "CircleCI read-write key", "key": "'"$(cat /tmp/${PACK}_rsa.pub)"'", "read_only": false}' \
+	"https://api.github.com/repos/${EXCHANGE_ORG}/${REPO_NAME}/keys"
 
 # GitHub: create a user-scope token
-curl -X POST --header "Content-Type: application/json" \
-	-d '{"scopes": ["repo"], "note": "CircleCI: ${REPO_NAME} token"}' \
-	POST https://api.github.com/authorizations \
-	| jq ".token" > /tmp/${PACK}_user_token
+curl -sS --fail -u "${USERNAME}:${PASSWORD}" -X POST --header "Content-Type: application/json" \
+	-d '{"scopes": ["public_repo"], "note": "CircleCI: '"${REPO_NAME}"'"}' \
+	"https://api.github.com/authorizations" | jq ".token" > /tmp/${PACK}_user_token
+
+if [ ! -s /tmp/${PACK}_user_token ];
+then
+	echo "Could not create a token."
+	exit 1
+fi
+
+# Git: push circle.yml
+curl -sS --fail "https://raw.githubusercontent.com/StackStorm-Exchange/ci/master/.circle/circle.yml.sample" > circle.yml
+git add circle.yml
+git commit -m 'Bootstrap a StackStorm Exchange pack repository.'
+git push origin master
 
 # CircleCI: follow the project
-curl -X POST https://circleci.com/api/v1.1/project/github/${EXCHANGE_ORG}/${REPO_NAME}/follow?circle-token=${CIRCLECI_TOKEN}
+curl -sS --fail -X POST "https://circleci.com/api/v1.1/project/github/${EXCHANGE_ORG}/${REPO_NAME}/follow?circle-token=${CIRCLECI_TOKEN}"
 
 # CircleCI: upload the read-write key
-curl -X POST --header "Content-Type: application/json" \
-	-d '{"hostname":"github.com","private_key":"$(cat /tmp/${PACK}_rsa)"}' \
-	https://circleci.com/api/v1.1/project/github/${EXCHANGE_ORG}/${REPO_NAME}/ssh-key?circle-token=${CIRCLECI_TOKEN}
+curl -sS --fail -X POST --header "Content-Type: application/json" \
+	-d '{"hostname":"github.com","private_key":"'"$(cat /tmp/${PACK}_rsa)"'"}' \
+	"https://circleci.com/api/v1.1/project/github/${EXCHANGE_ORG}/${REPO_NAME}/ssh-key?circle-token=${CIRCLECI_TOKEN}"
 
 # CircleCI: specify the credentials (the machine login and the new user-scope token)
-curl -X POST --header "Content-Type: application/json" \
-	-d '{"name":"MACHINE_USER", "value":"${USERNAME}"}' \
-	https://circleci.com/api/v1.1/project/github/${EXCHANGE_ORG}/${REPO_NAME}/envvar?circle-token=${CIRCLECI_TOKEN}
-curl -X POST --header "Content-Type: application/json" \
-	-d '{"name":"MACHINE_PASSWORD", "value":"$(cat /tmp/${PACK}_user_token)"}' \
-	https://circleci.com/api/v1.1/project/github/${EXCHANGE_ORG}/${REPO_NAME}/envvar?circle-token=${CIRCLECI_TOKEN}
+curl -sS --fail -X POST --header "Content-Type: application/json" \
+	-d '{"name":"MACHINE_USER", "value":"'"${USERNAME}"'"}' \
+	"https://circleci.com/api/v1.1/project/github/${EXCHANGE_ORG}/${REPO_NAME}/envvar?circle-token=${CIRCLECI_TOKEN}"
+curl -sS --fail -X POST --header "Content-Type: application/json" \
+	-d '{"name":"MACHINE_PASSWORD", "value":'"$(cat /tmp/${PACK}_user_token)"'}' \
+	"https://circleci.com/api/v1.1/project/github/${EXCHANGE_ORG}/${REPO_NAME}/envvar?circle-token=${CIRCLECI_TOKEN}"
 
 # Clean up
 rm -rf ${REPO_DIR} /tmp/${PACK}_rsa* /tmp/${PACK}_user_token
