5fe6e841ab93c583ef1ddf037b91c63a543ca4bf
Tomaz Muraus <tomaz@tomaz.me>
2016-10-20 15:41:53 +0200

    Make sure we always process pack metadata files in the same orders and also sort the serialized JSON so the generated file is stable and doesn't changes with the same input data.

--- .circle/index.py
+++ .circle/index.py
@@ -2,22 +2,27 @@ import json
 import yaml
 import sys
 from glob import glob
+from collections import OrderedDict
 
 EXCHANGE_NAME = "StackStorm-Exchange"
 EXCHANGE_PREFIX = "stackstorm"
 
 
 def build_index(path):
-    packs = {}
-    for filename in glob('%s/packs/*.yaml' % path):
+    packs = OrderedDict()
+
+    generator = sorted(glob('%s/packs/*.yaml' % path))
+    for filename in generator:
         with open(filename, 'r') as pack:
             pack_meta = yaml.load(pack)
+
         pack_meta['repo_url'] = 'https://github.com/%s/%s-%s' % (
             EXCHANGE_NAME, EXCHANGE_PREFIX, pack_meta['name']
         )
         packs[pack_meta['name']] = pack_meta
+
     with open('%s/index.json' % path, 'w') as outfile:
-        json.dump(packs, outfile, indent=4)
+        json.dump(packs, outfile, indent=4, sort_keys=True)
 
 if __name__ == '__main__':
     path = sys.argv[1]
