29ae86c3e6fdaef740c8756ed18387f5205bf034
Tomaz Muraus <tomaz@tomaz.me>
2016-11-07 16:12:59 +0100

    Re-use pack schema from st2 so we don't have copy and pasted code all over which is a pita to maintain.

--- .circle/validate.py
+++ .circle/validate.py
@@ -15,10 +15,13 @@
 
 import sys
 
-import jsonschema
 import yaml
 
+from st2common.models.api.pack import PackAPI
+from st2common.util import schema as util_schema
+
 PREFIX = 'stackstorm'
+PACK_SCHEMA = PackAPI.schema
 
 
 def load_yaml_file(path):
@@ -29,7 +32,10 @@ def load_yaml_file(path):
 
 
 def validate_schema(instance, schema):
-    return jsonschema.validate(instance, schema)
+    return util_schema.validate(instance=instance, schema=schema,
+                                cls=util_schema.CustomValidator,
+                                use_default=True,
+                                allow_default_none=True)
 
 
 def validate_repo_name(instance, repo_name):
@@ -39,9 +45,8 @@ def validate_repo_name(instance, repo_name):
 if __name__ == '__main__':
     repo_name = sys.argv[1]
     pack_meta = load_yaml_file(sys.argv[2])
-    pack_schema = load_yaml_file(sys.argv[3])
 
-    validate_schema(pack_meta, pack_schema)
+    validate_schema(pack_meta, PACK_SCHEMA)
     validate_repo_name(pack_meta, repo_name)
 
     print pack_meta['name']
