9d78f058f2035b1b047a27e5a8b0fe521f0b319c
Jacob Floyd <cognifloyd@gmail.com>
2021-09-20 22:49:21 -0500

    Create new pack repos with stackstorm-neptr

--- utils/exchange-bootstrap.sh
+++ utils/exchange-bootstrap.sh
@@ -60,7 +60,9 @@ rm -rf "${REPO_DIR}" "/tmp/${PACK}_rsa*"
 mkdir -p "${REPO_DIR}" && cd "${REPO_DIR}"
 # TODO: switch to main as default branch
 git init --initial-branch master \
-  && git remote add origin "${REPO_URL}"
+  && git remote add origin "${REPO_URL}" \
+  && git config --local user.name "${GITHUB_USERNAME}" \
+  && git config --local user.email "info@stackstorm.com"
 
 # Generate a keypair
 #echo "Generating random private SSH key"
